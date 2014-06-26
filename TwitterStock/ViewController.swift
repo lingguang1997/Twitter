fe1a80e6e2d7679e841535a2acf39b3493be2863
Zijiao Liu <zliu@twitter.com>
2014-06-26 15:17:01 -0700

    Initial Commit

---
+++ TwitterStock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterStock
+//
+//  Created by Zijiao Liu on 6/26/14.
+//  Copyright (c) 2014 Zijiao Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
